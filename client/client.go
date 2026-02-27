e3edda742820ef8869e6bf64a1fbf73c87f5bdd6
nacl <naclwww@outlook.com>
2026-02-27 22:27:21 +0800

    完成隧道初步验证

--- client/client.go
+++ client/client.go
@@ -8,116 +8,189 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"log/slog"
 	"net"
 	"net/http"
-	"net/netip"
-	"net/url"
-	"strconv"
+	"os"
+	"strings"
 )
 
-var targetAddr = netip.MustParseAddrPort("111.63.65.247:80")
+var ServerAddr = "http://172.16.48.210:5000"
+
+type PySocket struct {
+	addr     net.Addr
+	id       string
+	conn     net.Conn
+	done     chan struct{}
+	respChan chan *http.Response
+}
+
+func NewPySocket(c net.Conn) *PySocket {
+	var r [16]byte
+	rand.Read(r[:])
+	s := &PySocket{
+		addr:     c.RemoteAddr(),
+		id:       "_" + hex.EncodeToString(r[:]),
+		conn:     c,
+		done:     make(chan struct{}),
+		respChan: make(chan *http.Response, 16),
+	}
+	return s
+}
+
+func (p *PySocket) Close() error {
+	close(p.done)
+	return p.conn.Close()
+}
 
 func main() {
 	l, err := net.Listen("tcp", "127.0.0.1:2345")
 	if err != nil {
 		log.Fatalln(err)
 	}
+	Init()
+
+	in := make(chan *PySocket, 16)
+
+	go handler(in)
 
 	for {
 		conn, err := l.Accept()
 		if err != nil {
 			log.Fatalln(err)
+			l.Close()
+			return
 		}
-		s := NewPySocket(targetAddr, func(s string) (string, error) {
-			resp, err := http.Get(fmt.Sprintf("http://127.0.0.1:5000?code=%s", url.QueryEscape(s)))
-			if err != nil {
-				log.Fatalln(err)
+		py := NewPySocket(conn)
+		in <- py
+	}
+}
+
+func eval(code string) (resp *http.Response, err error) {
+	res, err := http.Get(fmt.Sprintf("%s?code=%s", ServerAddr, code))
+	if err != nil {
+		log.Println("http request error:", err)
+		return res, err
+	}
+	return res, nil
+}
+
+func Init() {
+	response, err := eval(encode())
+	if err != nil {
+		log.Fatalln("init error:", err)
+	}
+	response.Body.Close()
+}
+
+func encode() string {
+	readfile := "proxy.py"
+	content, _ := os.ReadFile(readfile)
+	encoded := strings.ReplaceAll(string(content), "\r\n", "\n")
+	encoded = strings.ReplaceAll(encoded, "\n", "\\n")
+	encoded = strings.ReplaceAll(encoded, "\"", "\\\"")
+	fmt.Println(encoded)
+	return encoded
+}
+
+func handler(in chan *PySocket) {
+	for {
+		select {
+		case py, ok := <-in:
+			if !ok {
+				return // 通道已关闭，退出函数
 			}
-			if resp.StatusCode != 200 {
-				return "", errors.New("code not 200")
+			go py.Read()
+			go py.Write()
+		}
+	}
+}
+
+func (p *PySocket) Write() {
+	buf := make([]byte, 4096)
+	defer p.Close()
+	defer close(p.done)
+	for {
+		n, err := p.conn.Read(buf)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				log.Println("connection closed by remote")
+				return
+			} else {
+				log.Println("read error:", err)
+				return
 			}
+		}
+		data := buf[:n]
+		host, port := Parser(data)
+		code := fmt.Sprintf("connect(%s,%s,%d,%s)", p.id, host, port, base64.StdEncoding.EncodeToString(data))
+		resp, err := http.Get(fmt.Sprintf("%s?code=%s", ServerAddr, code))
+		if err != nil {
+			log.Println("http request error:", err)
+			continue
+		}
+		p.respChan <- resp
+	}
+}
+
+// Parser return Host and Port from data
+func Parser(data []byte) (string, int) {
+	if len(data) < 10 || data[0] != 0x05 {
+		return "", 0 // 不是SOCKS5协议
+	}
+
+	if data[1] != 0x01 { // CMD不是CONNECT
+		return "", 0
+	}
+
+	atyp := data[3]
+	var addr string
+	var portStart int
+
+	switch atyp {
+	case 0x01: // IPv4
+		if len(data) < 10 {
+			return "", 0
+		}
+		addr = fmt.Sprintf("%d.%d.%d.%d", data[4], data[5], data[6], data[7])
+		portStart = 8
+	case 0x03: // 域名
+		addrLen := int(data[4])
+		if len(data) < 5+addrLen+2 {
+			return "", 0
+		}
+		addr = string(data[5 : 5+addrLen])
+		portStart = 5 + addrLen
+	case 0x04: // IPv6
+		return "", 0 // 简化处理，先不支持IPv6
+	default:
+		return "", 0
+	}
+
+	port := int(data[portStart])<<8 | int(data[portStart+1])
+	return addr, port
+}
+
+func (p *PySocket) Read() {
+	for {
+		select {
+		case resp := <-p.respChan:
 			data, err := io.ReadAll(resp.Body)
 			if err != nil {
-				return "", err
+				log.Println("read response body error:", err)
+				continue
 			}
-			return string(data), nil
-		})
-		go func() {
-			buf := make([]byte, 1024)
-			for {
-				n, err := conn.Read(buf)
-				if err != nil && !errors.Is(err, io.EOF) {
-					slog.Info("read from local fail", "err", err)
-					break
-				}
-				buf = buf[:n]
-				err = s.Write(buf)
-				if err != nil {
-					slog.Info("write to remote fail", "err", err)
-					break
-				}
+			decoded, err := base64.StdEncoding.DecodeString(string(data))
+			if err != nil {
+				log.Println("base64 decode error:", err)
+				continue
 			}
-		}()
-		go func() {
-			for {
-				buf := make([]byte, 1024)
-				n, err := s.Read(buf)
-				if err != nil {
-					slog.Info("read from remote fail", "err", err)
-					break
-				}
-				_, err = conn.Write(buf[:n])
-				if err != nil {
-					slog.Info("write to local fail", "err", err)
-					break
-				}
+			_, err = p.conn.Write(decoded)
+			if err != nil {
+				log.Println("write to conn error:", err)
+				continue
 			}
-		}()
+		case <-p.done:
+			return
+		}
 	}
-}
-
-type PySocket struct {
-	remoteName string
-	eval       func(string) (string, error)
-}
-
-func NewPySocket(addr netip.AddrPort, eval func(string) (string, error)) *PySocket {
-	var r [16]byte
-	rand.Read(r[:])
-	s := &PySocket{
-		remoteName: "_" + hex.EncodeToString(r[:]),
-		eval:       eval,
-	}
-	eval(fmt.Sprintf("exec('__builtins__[\"%s\"] = __import__(\"socket\").socket(__import__(\"socket\").AF_INET, __import__(\"socket\").SOCK_STREAM)')", s.remoteName))
-	eval(fmt.Sprintf("__builtins__['%s'].connect(('%s', %d))", s.remoteName, addr.Addr().String(), addr.Port()))
-	return s
-}
-
-func (p *PySocket) Write(d []byte) error {
-	encoded := base64.StdEncoding.EncodeToString(d)
-	n, err := p.eval(fmt.Sprintf("__builtins__['%s'].send(__import__('base64').b64decode(b'%s'))", p.remoteName, encoded))
-	if err != nil {
-		return err
-	}
-	sendN, err := strconv.Atoi(n)
-	if err != nil {
-		return err
-	}
-	slog.Info("send", "len", sendN)
-	return nil
-}
-
-func (p *PySocket) Read(buf []byte) (int, error) {
-	length := len(buf)
-	res, err := p.eval(fmt.Sprintf("__import__('base64').b64encode(__builtins__['%s'].recv(%d)).decode('utf-8')", p.remoteName, length))
-	if err != nil {
-		return 0, err
-	}
-	n, err := base64.StdEncoding.Decode(buf, []byte(res))
-	if err != nil {
-		return n, err
-	}
-	slog.Info("read", "len", n)
-	return n, nil
 }
